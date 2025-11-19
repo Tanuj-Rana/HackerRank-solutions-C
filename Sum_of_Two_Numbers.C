c77640e2ba92494e649460fb805a4c5ec4fc3e11
Tanuj-Rana <tanujrana581@gmail.com>
2025-11-19 20:53:24 +0530

    Add Sum_of_Two_ Numbers solution

---
+++ Sum_of_Two_Numbers.C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int a,b;
+   scanf("%d",&a);
+   scanf("%d",&b);
+   int i=(a+b);
+    printf( "The sum of %d and %d is %d.", a,b,i);
+    return 0;
+}
