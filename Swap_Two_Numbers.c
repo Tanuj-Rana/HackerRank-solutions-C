d032215bd2f98d9a18e8082c75c88828c21c3852
Tanuj-Rana <tanujrana581@gmail.com>
2025-12-08 09:52:06 +0530

    Add Swap Two Numbers

---
+++ Swap_Two_Numbers.c
@@ -0,0 +1,27 @@
+/*
+Write a program to swap two numbers.
+
+Input Format
+
+Two unique integers a and b
+
+Constraints
+
+-10^6 <= a <= 10^6
+-10^6 <= b <= 10^6
+Output Format
+
+Two lines printing the swapped values of a and b*/
+
+#include <stdio.h>
+
+int main() {
+   int a,b,c;
+    scanf("%d %d",&a,&b);
+    c=a;
+    a=b;
+    b=c;
+    printf("a = %d\n",a);
+    printf("b = %d",b);
+    return 0;
+}
