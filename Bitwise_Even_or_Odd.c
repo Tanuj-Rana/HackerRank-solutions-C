76d1f10ae5faa3b07404e0e4982d7da0774f5713
Tanuj-Rana <tanujrana581@gmail.com>
2025-12-08 09:48:38 +0530

    Add Bitwise_Even or Odd

---
+++ Bitwise_Even_or_Odd.c
@@ -0,0 +1,31 @@
+/*
+Write a program to determine whether a given integer N is even or odd using only bitwise operators.
+
+The program should not use the remainder (%) operator.
+
+Input Format
+
+A single integer N
+Constraints
+
+-10^5 ≤ N ≤ 10^5
+Output Format
+
+Print Even if N is an even number.
+Print Odd if N is an odd number.*/
+
+#include <stdio.h>
+
+int main() {
+
+    int N;
+    scanf("%d",&N);
+    if((N&1)==0)
+    {
+        printf("Even");  
+    }
+    else{
+        printf("Odd");
+    }
+    return 0;
+}
